dd15905901008c4bc6aa4c5e8018d1a789373439
Egor <Egor@DESKTOP-QGB3P2I>
2020-05-03 18:39:08 +0100

    some mobile work

---
+++ Scripts/myscript.js
@@ -0,0 +1,27 @@
+var navButtonState = 0;
+$(document).ready(function(){
+    $("#forTransform").hide();
+})
+
+
+function displayMobileNavMenu(){
+   
+    if(navButtonState == 0)
+    {
+        
+        navButtonState = 1;
+        $(document).ready(function(){
+            $("#forTransform").show();
+            
+            
+        })
+    }
+    else{
+        navButtonState = 0;
+        $(document).ready(function(){
+            $("#forTransform").hide();
+            
+            
+        })
+    }
+}
