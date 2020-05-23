dbc899fbb7d36b365a10bf288b7ad33ea539ea3c
Egor <Egor@DESKTOP-QGB3P2I>
2020-05-23 14:24:30 +0100

    updated js

--- Scripts/myscript.js
+++ Scripts/myscript.js
@@ -27,5 +27,31 @@ function displayMobileNavMenu(){
 }
 
 function expandProject(prjID){
+    
+    var str = prjID;
+    var num = Number(str.replace(/prj/i,""));
+    var implementedStrings = ["C#, using WPF as the interface framework","Something"];
+    var featuresForProjects = [["AI","Save/Load","Archive of Played Games","Fischer Random Chess"],["something","something","something","something","something"]];
+    var githubLinks = ["https://github.com/egorchern/Project_ChessWithInterface","https://github.com/PathOfBuildingCommunity/PathOfBuilding"];
+    var descriptions = [
+        "Chess",
+        "Something"
+    ]
+    $(document).ready(new function(){
+        $("#implemented").html(implementedStrings[num]);
+        var featureList = featuresForProjects[num];
+        $(".featureGrid").empty();
+        for(var i = 0; i < featureList.length; i++)
+        {
+            $(".featureGrid").append("<p>" + featureList[i] + "</p>");
+        }
+        $("#githubLink").attr("href",githubLinks[num]);
+        $("#descr").empty();
+        $("#descr").append("<p>" + descriptions[num] + "</p>");
+        $("#detailedPrjGrid").css("display","grid");
+    })
+    
+    
+
    
 }
