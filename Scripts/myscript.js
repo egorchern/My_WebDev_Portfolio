c5a7bc2ed4081e0177f984b30d1288d1019f12b5
Egor Chernyshev <egorch.formal@gmail.com>
2021-02-07 13:23:07 +0000

    added two more projects

--- Scripts/myscript.js
+++ Scripts/myscript.js
@@ -151,7 +151,7 @@ function expandProject(prjID) {
 
     var str = prjID;
     var num = Number(str.replace(/prj/i, ""));
-    var implementedStrings = ["C#, using WPF as the interface framework", "HTML + CSS(Bootsrap included) + JS(jQuery)", "HTML + CSS(Bootsrap included) + JS(jQuery)", "Flask(Python) + HTML + CSS(Bootsrap included) + JS(jQuery)", "C#, using WPF as the interface framework", "HTML + CSS(Bootsrap included) + JS(jQuery)", "HTML + CSS(Bootsrap included) + JS", "HTML + JS", "HTML + CSS(Bootsrap included) + JS"];
+    var implementedStrings = ["C#, using WPF as the interface framework", "HTML + CSS(Bootsrap included) + JS(jQuery)", "HTML + CSS(Bootsrap included) + JS(jQuery)", "Flask(Python) + HTML + CSS(Bootsrap included) + JS(jQuery)", "C#, using WPF as the interface framework", "HTML + CSS(Bootsrap included) + JS(jQuery)", "HTML + CSS(Bootsrap included) + JS", "HTML + JS", "HTML + CSS(Bootsrap included) + JS", "HTML + CSS(Bootstrap included) + JS(React)", "HTML + CSS(Bootstrap included) + JS(React)"];
     var featuresForProjects = [
         ["AI", "Save/Load", "Archive of Played Games", "Fischer Random Chess"],
         ["Dynamic addition of bookmarks", "Responsive", "Visual layering of bookmarks"],
@@ -161,7 +161,9 @@ function expandProject(prjID) {
         ["Animations on scroll","Responsive", "Fast"],
         ["Help page", "Hold piece feature", "Modern interface"],
         ["No external libraries", "Performance"],
-        ["Dark theme", "Autosave", "Single-page", "Import && Export"]
+        ["Dark theme", "Autosave", "Single-page", "Import && Export"],
+        ["Single page", "Reactful", "AI"],
+        ["No libraries used for graphing or algorithms", "Responsive design", "Efficient algorithms"]
     ];
     let slide_show_lists = [
         ["Assets/prj1_slide1.webp", "Assets/prj1_slide2.webp", "Assets/prj1_slide3.webp"],
@@ -172,9 +174,11 @@ function expandProject(prjID) {
         ["Assets/prj6_slide1.webp", "Assets/prj6_slide2.webp","Assets/prj6_slide3.webp" ],
         ["Assets/prj7_slide1.webp", "Assets/prj7_slide2.webp"],
         ["Assets/prj8_slide1.webp", "Assets/prj8_slide2.webp"],
-        ["Assets/prj9_slide1.webp", "Assets/prj9_slide2.webp", "Assets/prj9_slide3.webp"]
+        ["Assets/prj9_slide1.webp", "Assets/prj9_slide2.webp", "Assets/prj9_slide3.webp"],
+        ["Assets/prj10_slide1.webp", "Assets/prj10_slide2.webp", "Assets/prj10_slide3.webp"],
+        ["Assets/prj11_slide1.webp", "Assets/prj11_slide2.webp", "Assets/prj11_slide3.webp", "Assets/prj11_slide4.webp", "Assets/prj11_slide5.webp"]
     ];
-    var githubLinks = ["https://github.com/egorchern/Project_ChessWithInterface", "https://github.com/egorchern/bookmarker", "https://github.com/egorchern/minesweeper", "https://github.com/egorchern/Compare_flask", "https://github.com/egorchern/Simplex_Solver", "https://github.com/egorchern/MyTutor", "https://github.com/egorchern/Tetris", "https://github.com/egorchern/Snake"];
+    var githubLinks = ["https://github.com/egorchern/Project_ChessWithInterface", "https://github.com/egorchern/bookmarker", "https://github.com/egorchern/minesweeper", "https://github.com/egorchern/Compare_flask", "https://github.com/egorchern/Simplex_Solver", "https://github.com/egorchern/MyTutor", "https://github.com/egorchern/Tetris", "https://github.com/egorchern/Snake", "https://github.com/egorchern/Nought_and_Crosses_React"];
     var descriptions = [
         "This application was made with WPF which is the modern framework for developing desktop apps with C languages. My chess app includes a choice to have control over both sides or play against an AI which utilizes parallel processing. Everything has been done from scratch except for the visual assets which were taken from royalty-free image website. This project also includes an interfacing app to connect to an offline database. Every completed game automatically gets recorded in the database so that the user can analyze them later. The games can be queried for several parameters that can be mixed together to find the intended game(s)",
         "You can have a look at the website by clicking <a href='https://egorchern.github.io/bookmarker/' target='_black'>here</a><br>This project is a web site hosted on GitHub Pages.   website allows users to add a visual bookmark via a user-friendly interface which can be maximized or minimized by clicking an appropriate button again. The bookmark data is stored permanently via cookies. Bookmark tiles are then generated dynamically via JavaScript. Bookmarks can be easily deleted via another menu that can be expanded via the minus button at the top of the page. The site is minimalistic in its design, as it only includes the necessary details.",
@@ -184,7 +188,9 @@ function expandProject(prjID) {
         "You can have a look at the website by clicking <a href='https://egorchern.github.io/MyTutor/' target='_black'>here</a><br></br>This is a short, static, front-end focused website that I've created to promote my tutoring services. It is responsive and utilizes scroll track that then animates in page content. The pages are designed in a tile layout (one block element per tile). Also, the scroll animations make the user focus on every part of the webpage before reading next tile.",
         "You can have a look at the website by clicking <a href='https://egorchern.github.io/Tetris/' target='_black'>here</a><br></br>This is a remake of a classical game called Tetris. I remade it in browser without using any game engines or libraries (except for Bootstrap). The game logic is made using objects and the drawing is done via HTML5 canvas element. New shapes can easily be added by modifying shapes dictionary.",
         "You can have a look at the website by clicking <a href='https://egorchern.github.io/Snake/' target='_black'>here</a><br></br>This is a remake of classical game that was popular on early mobile phones - Snake. Implementation is made using html canvas and processing made at every frame. All rules follow the original ones, score is recorder at the top menu. Restart available without having to reload the page.",
-        "You can have a look at the website by clicking <a href='https://egorchern.github.io/Youtube_note_taker/' target='_black'>here</a><br></br>This website allows you to make notes on YouTube videos side by side. You can then see notes recorder to the right of the video, by clicking on them you will skip to the time that note was taken, this way notes can be reviset and YouTube video slice watched again for revision. All of the data is stored client-side and autosaved. Import or Export data is implemented as well."
+        "You can have a look at the website by clicking <a href='https://egorchern.github.io/Youtube_note_taker/' target='_black'>here</a><br></br>This website allows you to make notes on YouTube videos side by side. You can then see notes recorder to the right of the video, by clicking on them you will skip to the time that note was taken, this way notes can be reviset and YouTube video slice watched again for revision. All of the data is stored client-side and autosaved. Import or Export data is implemented as well.",
+        "You can have a look at the website by clicking <a href='https://egorchern.github.io/Nought_and_Crosses_React/' target='_black'>here</a><br></br>This is the website I made when I started using React.js, it is a simple Nought-and-Crosses game with an AI made with Minimax algorithm. Due to simple complexity of the game, the AI is unbetable. Game can be quickly restarted by pressing R when the current game has ended.",
+        "You can have a look at the website by clicking <a href='https://egorchern.github.io/GraphMaster/' target='_black'>here</a><br></br>I mainly did this website to improve my skills in React and to check myself when doing exam questions on graph algorithms for Further maths. This website is centered around creating and using algorithms on graphs. The GUI allows for easy creation of graphs with modern front-end design. Graphs can be visially examined after creation, this is achieved via HTML5 canvas. Nodes are positioned in n-gon using mathematical calculations. Algorithms can then be executed on the graph, and relevant edges highlighted to show the path."
     ];
 
     $(document).ready(function () {
